aef8096284644f36b5bab4363ef3566880ae9767
devadob <admir.obralija@tik.uni-stuttgart.de>
2024-03-22 10:03:12 +0100

    Minor refactorings for ILIAS 8 support.
    - Minor PHP8-related fixes
    - Remove unused plugin attributes

--- classes/class.ilViPLabEventPlugin.php
+++ classes/class.ilViPLabEventPlugin.php
@@ -10,96 +10,63 @@ include_once './Services/EventHandling/classes/class.ilEventHookPlugin.php';
  */
 class ilViPLabEventPlugin extends ilEventHookPlugin
 {
-	private static $instance = null;
+    private static $instance = null;
+    const PLUGIN_NAME = 'ViPLabEvent';
+    const PLUGIN_ID = 'viplabevent';
 
-	const PNAME = 'ViPLabEvent';
-	const CTYPE = 'Services';
-	const CNAME = 'EventHandling';
-	const SLOT_ID = 'evhk';
 
-	/**
-	 * Get singelton instance
-	 * @global ilPluginAdmin $ilPluginAdmin
-	 * @return ilFhoevImportPlugin
-	 */
-	public static function getInstance()
-	{
+    public static function getInstance()
+    {
         if (self::$instance) {
             return self::$instance;
         }
 
         global $DIC;
+        $component_factory = $DIC["component.factory"];
+        return self::$instance = $component_factory->getPlugin(self::PLUGIN_ID);
+    }
 
-        $component_factory = $DIC['component.factory'];
-        $instance = $component_factory->getPlugin('viplabevent');
+    public function handleEvent($a_component, $a_event, $a_parameter): void
+    {
+        ilLoggerFactory::getLogger('viplabevent')->debug('New event: '. $a_event.' from component: ' .$a_component);
 
-        return self::$instance = $instance;
-	}
-	
-	/**
-	 * Handle event
-	 * @param type $a_component
-	 * @param type $a_event
-	 * @param type $a_parameter
-	 */
-	public function handleEvent($a_component, $a_event, $a_parameter): void
-	{
-		ilLoggerFactory::getLogger('viplabevent')->debug('New event: '. $a_event.' from component: ' .$a_component);
-		
-		if ($a_component == 'Services/WebServices/ECS' and $a_event == 'newEcsEvent') {
-            $plugin = ilassViPLabPlugin::getInstance();
-            $plugin->handleEcsEvent($a_event, $a_parameter);
-		}
-	}
-	
+        $assViPLabPlugin = ilassViPLabPlugin::getInstance();
 
-	/**
-	 * Get plugin name
-	 * @return string
-	 */
-	public function getPluginName(): string
-	{
-		return self::PNAME;
-	}
+        if ($assViPLabPlugin->isActive()) {
+            $assViPLabPlugin->handleEcsEvent($a_event, $a_parameter);
+        }
+    }
 
-	/**
-	 * Init auto load
-	 */
-	protected function init(): void
-	{
-		$this->initAutoLoad();
-	}
+    public function getPluginName(): string
+    {
+        return self::PLUGIN_NAME;
+    }
 
-	/**
-	 * Init auto loader
-	 * @return void
-	 */
-	protected function initAutoLoad()
-	{
-		spl_autoload_register(
-				array($this, 'autoLoad')
-		);
-	}
-	
-	
+    protected function init(): void
+    {
+        $this->initAutoLoad();
+    }
 
-	/**
-	 * Auto load implementation
-	 *
-	 * @param string class name
-	 */
-	private final function autoLoad($a_classname)
-	{
-		$class_file = $this->getClassesDirectory() . '/class.' . $a_classname . '.php';
-		if (@include_once($class_file))
-		{
-			return;
-		}
-	}
+    protected function initAutoLoad()
+    {
+        spl_autoload_register(array($this, 'autoLoad'));
+    }
+
+    /**
+     * Auto load implementation
+     *
+     * @param string class name
+     */
+    private function autoLoad($a_classname)
+    {
+        $class_file = $this->getClassesDirectory() . '/class.' . $a_classname . '.php';
+        if (@include_once($class_file)) {
+            return;
+        }
+    }
 
     protected function getClassesDirectory() : string
     {
         return $this->getDirectory() . "/classes";
     }
-
 }
--- plugin.php
+++ plugin.php
@@ -1,15 +1,12 @@
 <?php
-
 // alphanumerical ID of the plugin; never change this
 $id = "viplabevent";
-
 // code version; must be changed for all code changes
 $version = "6.0.0";
-
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "8.0";
-$ilias_max_version = "8.999";
+$ilias_min_version = "8.0.0";
+$ilias_max_version = "8.999.999";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Per Pascal Seeland"; 
